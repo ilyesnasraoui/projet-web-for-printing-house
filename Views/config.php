99a28597d1a1bbe6a07e8a1adbc2f9ce873b594f
kaissounetahfoun <kais.lamine@esprit.tn>
2019-12-11 18:12:59 +0100

    sq

--- Views/config.php
+++ Views/config.php
@@ -10,11 +10,11 @@ class config
         {
             try
             {
-<<<<<<< HEAD
+
                 self::$instance = new PDO('mysql:host=localhost;dbname=data', 'root', '');
-=======
+
                 self::$instance = new PDO('mysql:host=localhost;dbname=s_i_a_d', 'root', '');
->>>>>>> 64b390bb6a25b6741daf6b52224e63e6595d0cba
+
                 self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }
             catch(Exception $e)
